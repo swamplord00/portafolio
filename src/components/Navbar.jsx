9f756b8e21b17f3c85d2e8ff95b991f95e6ef7c7
Victor Briones <vbrionesparra@gmail.com>
2023-09-25 16:42:10 -0300

    navbar responsive hambuger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,128 @@
-import Link from 'next/link'
-import React from 'react'
-import { DribbbleIcon, GitHubIcon, LinkedInIcon, PinterestIcon, XIcon } from './Icons'
+"use client"
+import Link from "next/link";
+import React, { useState } from "react";
+import {
+  DribbbleIcon,
+  GitHubIcon,
+  LinkedInIcon,
+  PinterestIcon,
+  XIcon,
+} from "./Icons";
 
 export const Navbar = () => {
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <>
-       <header className='w-full px-2 py-8 font-medium flex items-center justify-between'>
-            <nav className='space-x-2 flex flex-wrap'>
-                
-                <Link href="/" >Home</Link>
-                <Link href="/about" >About</Link>
-                <Link href="/projects" >Projects</Link>
-                
 
-            </nav>
-            <div className='w-16 h-16 bg-black text-white flex items-center justify-center font-bold text-2xl rounded-full'>
-                VB
+
+      <div className="flex items-center justify-between border-b border-gray-400 py-8 px-10" >
+      <nav>
+
+      <Link href="/">
+      <div className="w-16 h-16 bg-black text-white flex items-center justify-center font-bold text-2xl rounded-full">
+          VB
+      </div>
+      </Link>
+        
+      </nav>
+      <nav className="flex items-center justify-center flex-wrap gap-2 ">
+          <a href="https://x.com " target={"_blank"}>
+            <XIcon />
+          </a>
+          <a href="https://github.com/swamplord00" target={"_blank"}>
+            <GitHubIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/victor-briones-82324558"
+            target={"_blank"}
+          >
+            <LinkedInIcon />
+          </a>
+          <a href="https://pinterest.com" target={"_blank"}>
+            <PinterestIcon />
+          </a>
+          <a href="https://dribbble.com" target={"_blank"}>
+            <DribbbleIcon />
+          </a>
+        </nav>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
             </div>
-            <nav className='flex items-center justify-center flex-wrap gap-2 '>
-                <a href='https://x.com ' target={'_blank'}><XIcon/></a>
-                <a href='https://github.com/swamplord00' target={'_blank'}><GitHubIcon/></a>
-                <a href='https://www.linkedin.com/in/victor-briones-82324558' target={'_blank'}><LinkedInIcon/></a>
-                <a href='https://pinterest.com' target={'_blank'}><PinterestIcon/></a>
-                <a href='https://dribbble.com' target={'_blank'}><DribbbleIcon/></a>
+            <ul className="flex flex-col items-center justify-between min-h-[250px]">
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/about">About</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/portfolio">Portfolio</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </section>
 
-            </nav>
-
-       </header>
-    
-    
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+          <li>
+          <Link href="/about">About</Link>
+          </li>
+          <li>
+          <Link href="/projects">Portfolio</Link>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+    </div>
+      
     </>
-  )
-}
+  );
+};
